fd41528ec54165274e9568207221c3ae44c2507b
Samantha Curatolo <85077075+scuratolo26@users.noreply.github.com>
2022-06-29 14:32:47 -0400

    validate password length

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,23 @@
 // Assignment code here
+function generatePassword() {
+  // ask password length 
+  const passwordLength = prompt("Enter a number between 8 and 128", "8");
+  console.log(passwordLength);
 
+  // validate password length 
+  if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("You need to provide a valid answer! Please try again.");
+    return generatePassword();
+  } else if (passwordLength === "" || passwordLength === null) {
+    window.alert("You need to provide a valid answer! Please try again.");
+    return generatePassword();
+  } else if (isNaN(passwordLength)) {
+    window.alert("You need to provide a valid answer! Please try again.");
+    return generatePassword();
+  }
+
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
